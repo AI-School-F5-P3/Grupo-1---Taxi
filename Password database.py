82ac867eeb356f9964d4da82c63f8057ed3fb297
carrillo-p <alb.carrillop@gmail.com>
2024-06-17 14:42:31 +0200

    comentarios

--- Password database.py
+++ Password database.py
@@ -2,10 +2,20 @@ import hashlib
 import pandas as pd
 import numpy as np
 
-lista_usuarios = {'Usuarios': [], 'Passwords': []}
-datos_usuarios = pd.DataFrame.from_dict(lista_usuarios)
+lista_usuarios = {'Usuarios': [], 'Passwords': []} #Diccionario vacío para ir rellenando con los usuarios y contraseñas que genere la función
+datos_usuarios = pd.DataFrame.from_dict(lista_usuarios) #Se pasa del diccionario a una base de datos en pandas
 
 def LogIn():
+    '''
+    La función en primer lugar va a pedir un nombre al usuario y comprobará mediante condicional if si existe ese nombre en nuestra base de datos.
+    Si el usuario no existe, entendemos que es un usuario nuevo y le pedimos que nos de un nombre
+    La función comprueba que el nombre no esté ya en uso, y mientras no se de un nombre nuevo seguira pidiendolo en bucle.
+    Después se le pide una contraseña "segura", podríamos definir que significa que su contraseña sea segura e incluso solicitar una longitud concreta (o carácteres)
+    Una vez tenemos la contraseña, esta se hashea utilizando sha-256 y se almacena de forma encriptada en nuestra base de datos, para aumentar la seguridad.
+    Finalmente el usuario y su contraseña encriptada se añaden a la base de datos de usuarios.
+    Si el usuario si existe, se le pedirá que introduzca su contraseña y está se buscara en la base de datos en su forma encriptada.
+    Si hay coincidencias el programa de taximetro se inicia, si no, se le pide que repita.
+    '''
     usuario = input("Escriba su nombre de usuario: ")
     if usuario not in datos_usuarios.Usuarios.isin(usuario):
         print("Usuario no registrado.")
@@ -19,8 +29,14 @@ def LogIn():
     else:
         password_inp = input("Escriba su contraseña: ")
         password_inp_hash = hashlib.sha256(password_inp.encode('utf-8'))
-        if datos_usuarios.loc[datos_usuarios["Usuarios"] == usuario, 'Passwords'].iloc[0] != password_inp_hash:
+        intentos = 0
+        while(datos_usuarios.loc[datos_usuarios["Usuarios"] == usuario, 'Passwords'].iloc[0] != password_inp_hash):
             print("Contraseña incorrecta")
+            password_inp = input("Escriba su contraseña: ")
+            password_inp_hash = hashlib.sha256(password_inp.encode('utf-8'))
+            intentos += 1
+            if intentos == 6:
+                break
         else:
             print("Bienvenido")
             #taximetro()

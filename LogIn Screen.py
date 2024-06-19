e701924882221c9cf833f286f62212b499b26bdf
carrillo-p <alb.carrillop@gmail.com>
2024-06-19 09:53:17 +0200

    Añado botones

--- LogIn Screen.py
+++ LogIn Screen.py
@@ -6,7 +6,7 @@ pygame.init()
 #Tamaño de pantalla del juego
 screen = pygame.display.set_mode((800, 400))
 #Texto que figura en la ventana ejecutada
-pygame.display.set_caption('Taximetro Interactiv')
+pygame.display.set_caption('Taximetro Interactivo')
 
 #Reloj interno, para configurar la tasa de refreso del juego (fps)
 clock = pygame.time.Clock()
@@ -14,11 +14,21 @@ clock = pygame.time.Clock()
 first_screen = pygame.image.load('Graficos/road 2.jpg')
 #Boton de LogIn
 #Fuente para el boton de LogIn
-login_font = pygame.font.SysFont('Lucida Console', 50)
+login_font = pygame.font.SysFont('Lucida Console', 45)
 #Rectangulo para el boton, (posicion ancho, posicion alto, tamaño ancho, tamaño alto)
-login_button_rect = pygame.Rect(575, 100, 190, 70)
+login_button_rect = pygame.Rect(585, 70, 170, 62)
 #Superficie donde se coloca el boton
-login_text = login_font.render('Log In', True, 'red')
+login_text = login_font.render('Log In', True, (255, 0, 0))
+
+#Boton de registo
+reg_font = pygame.font.SysFont('Lucida Console', 45)
+reg_but_rect = pygame.Rect(560, 170, 220, 62)
+reg_text = reg_font.render('Registro', True, (255, 0, 0))
+
+#Boton de quit
+quit_font = pygame.font.SysFont('Lucida Console', 45)
+quit_but_rect = pygame.Rect(610, 270, 120, 62)
+quit_text = quit_font.render('Quit', True, (255, 0, 0))
 
 #Bucle de ejecución del juego
 while True:
@@ -27,17 +37,30 @@ while True:
             pygame.quit()
             exit()
         if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-            if login_button_rect.collidepoint(event.pos):
+            if quit_but_rect.collidepoint(event.pos):
                 pygame.quit()
                 exit()
+    
     a,b = pygame.mouse.get_pos()
     if login_button_rect.x <= a <= login_button_rect.x + 190 and login_button_rect.y <= b <= login_button_rect.y + 70:
-        pygame.draw.rect(screen, (180,180,180), login_button_rect)
+        pygame.draw.rect(screen, (91 ,23 ,202, 0.8), login_button_rect) #sin hover
     else:
-        pygame.draw.rect(screen, (110, 110, 110), login_button_rect)
+        pygame.draw.rect(screen, (110, 110, 110), login_button_rect) #con hover
+
+    if reg_but_rect.x <= a <= reg_but_rect.x + 210 and reg_but_rect.y <= b <= reg_but_rect.y + 70:
+        pygame.draw.rect(screen, (180, 180, 180), reg_but_rect)
+    else:
+        pygame.draw.rect(screen, (110, 110, 110), reg_but_rect)
+
+    if quit_but_rect.x <= a <= quit_but_rect.x + 210 and quit_but_rect.y <= b <= quit_but_rect.y + 70:
+        pygame.draw.rect(screen, (180, 180, 180), quit_but_rect)
+    else:
+        pygame.draw.rect(screen, (110, 110, 110), quit_but_rect)
 
     screen.blit(first_screen, (0, 0))
     screen.blit(login_text, (login_button_rect.x + 5, login_button_rect.y+5))
+    screen.blit(reg_text, (reg_but_rect.x + 5, reg_but_rect.y+5))
+    screen.blit(quit_text, (quit_but_rect.x + 5, quit_but_rect.y+5))
 
     pygame.display.update()
     clock.tick(60)

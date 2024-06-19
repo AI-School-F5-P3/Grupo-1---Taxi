9f5328d338fca21c53441ae28b82e2c129f158a9
carrillo-p <alb.carrillop@gmail.com>
2024-06-19 12:41:34 +0200

    Botones funcionales

--- IU Class.py
+++ IU Class.py
@@ -1,7 +1,7 @@
 import pygame
 from sys import exit
 
-S_Width, S_Height = 1920,1080
+S_Width, S_Height = 800,400
 FPS = 60
 
 class Game:
@@ -35,11 +35,38 @@ class Level:
         self.gameStateManager = gameStateManager
 
     def run(self):
-        login_screen = pygame.image.load('Graficos/out.png')
-        self.display.blit(login_screen, (0,0))
         keys = pygame.key.get_pressed()
+        mouse = pygame.mouse.get_pressed()
+        a,b = pygame.mouse.get_pos()
+        login_screen = pygame.image.load('Graficos/login.jpg')
+        font = pygame.font.SysFont('Lucida Console', 45)
+        color_font = (200, 245, 10, 1)
+        #Boton Login
+        login_button_rect = pygame.Rect(220, 50, 390, 55)
+        login_text = font.render('Iniciar Sesion', True, color_font)
+        pygame.draw.rect(login_screen, (91, 23, 202, 0.8), login_button_rect)
+        #Boton Sign Up
+        reg_button_rect = pygame.Rect(260, 150, 305, 55)
+        reg_text = font.render('Registrarse', True, color_font)
+        pygame.draw.rect(login_screen, (91, 23, 202, 0.8), reg_button_rect)
+        #Boton Quit
+        quit_button_rect = pygame.Rect(360, 250, 120, 55)
+        quit_text = font.render('Quit', True, color_font)
+        pygame.draw.rect(login_screen, (91, 23, 202, 0.8), quit_button_rect)
+        self.display.blit(login_screen, (0,0))
+        if quit_button_rect.x <= a <= quit_button_rect.x+120 and quit_button_rect.y <= b <= quit_button_rect.y+55:
+            pygame.draw.rect(self.display, (91 ,23 ,202, 0.8), quit_button_rect)
+        else:
+            pygame.draw.rect(self.display, (65, 0, 168, 0.9), quit_button_rect)   
+        self.display.blit(login_text, (login_button_rect.x + 5, login_button_rect.y+5))
+        self.display.blit(reg_text, (reg_button_rect.x + 5, reg_button_rect.y + 5))
+        self.display.blit(quit_text, (quit_button_rect.x + 5, quit_button_rect.y + 5))
         if keys[pygame.K_e]:
             self.gameStateManager.set_state('start')
+        if mouse[0]:
+            if quit_button_rect.collidepoint((a,b)):
+                self.gameStateManager.set_state('start')
+        
 
 class Start:
     def __init__(self, display, gameStateManager):
@@ -47,7 +74,7 @@ class Start:
         self.gameStateManager = gameStateManager
 
     def run(self):
-        first_screen = pygame.image.load('Graficos/001.jpg')
+        first_screen = pygame.image.load('Graficos/start.jpg')
         self.display.blit(first_screen, (0,0))
         keys = pygame.key.get_pressed()
         if keys[pygame.K_f]:
@@ -61,6 +88,12 @@ class gameStateManager:
     def set_state(self, state):
         self.currentState = state
         
+class Quit:
+    def __init__(self, gameStateManager):
+        self.gameStateManager = gameStateManager
+    def run(self):
+        a,b = pygame.mouse.get_pos()
+
 
 game = Game()
 game.run()

1ea2486fa3432e8dd28e0309507c7cfc1b01c1a3
carrillo-p <alb.carrillop@gmail.com>
2024-06-22 12:55:09 +0200

    class textbox

--- IU Class.py
+++ IU Class.py
@@ -143,5 +143,40 @@ class quit:
         pygame.quit()
         exit()
 
+class TextBox:
+    def __init__ (self, x, y, w, h, font):
+        self.rect = pygame.rect(x, y, w, h)
+        self.color = 'gray'
+        self.text = ""
+        self.font = pygame.font.SysFont('Lucida Console', 45)
+        self.txt_surface = font.render(self.text, True, 'black')
+        self.active = False
+
+    def handle_event(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if self.rect.collidepoint(event.pos):
+                self.active = not self.active
+            else:
+                self.active = False
+            self.color = 'black' if self.active else 'gray'
+        
+        if event.type == pygame.KEYDOWN:
+            if self.active:
+                if event.key == pygame.K_RETURN:
+                    print(self.text)
+                    self.text = ""
+                elif event.key == pygame.K_BACKSPACE:
+                    self.text += self.text[:-1]
+                else:
+                    self.text += event.unicode
+                self.text_surface = self.font.render(self.text, True, 'black')
+    def draw(self, screen):
+        screen.fill('white')
+        pygame.draw.rect(screen, self.color, self.rect, 2)
+        screen.blit(self.txt_surface, (self.rect.x + 5, self.rect.y + 5))
+        width = max(200, self.txt_surface.get_width() + 10)
+        self.rect.w = S_Width
+
+                    
 game = Game()
 game.run()

e69148e5929b686d9a134116e8dd8ead5977bd37
EmmaMontalbanA <emonalv@gmail.com>
2024-06-23 19:49:41 +0200

    renombrar funciones

--- initApp.py
+++ initApp.py
@@ -3,15 +3,19 @@ import hashlib
 import traceback
 import datetime
 
+# Especifica la ruta del archivo CSV donde se almacenan los datos de los usuarios.
 path = "Usuarios.csv"
 
-# Función para registrar acciones y errores
-def registrar_accion(mensaje):
+def register_action(mensaje):
+# Función para registrar acciones y errores. 
+# Registra mensajes de acciones y errores en un archivo de registro (registro.txt) con una marca de tiempo.
     with open("registro.txt", "a") as archivo:
         timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         archivo.write(f"{timestamp} - {mensaje}\n")
 
-def cargar_datos():
+def load_data():
+# Función que intenta cargar los datos del archivo CSV especificado en path. 
+# Si el archivo no existe, crea uno nuevo con las columnas Usuarios, Passwords, Pregunta Secreta y Respuesta Secreta.
     try:
         # Intentar leer el archivo CSV
         datos_usuarios = pd.read_csv(path)
@@ -25,113 +29,135 @@ def cargar_datos():
         datos_usuarios = pd.DataFrame(columns=['Usuarios', 'Passwords', 'Pregunta Secreta', 'Respuesta Secreta'])
     return datos_usuarios
 
-def registrar_usuario(datos_usuarios):
+def request_input(mensaje, validacion=None):
+# Solicita una entrada del usuario y valida la entrada usando una función de validación opcional
+    while True:
+        entrada = input(mensaje).strip()
+        if validacion and not validacion(entrada):
+            print("La contreña ha de tener como mínimo 8 caracteres. Inténtalo de nuevo.")
+        else:
+            return entrada
+
+def validate_password_format(contrasena):
+# Valida que la contraseña tenga al menos 8 caracteres
+    return len(contrasena) >= 8
+
+def request_password(mensaje):
+# Solicita una contraseña del usuario y valida que tenga al menos 8 caracteres
+    return request_input(mensaje, validate_password_format)
+
+def register_user(datos_usuarios):
+    ''' 
+    Esta función registra un nuevo usuario:
+    1. Solicita un nombre de usuario y verifica que no esté en uso.
+    2. Solicita una contraseña, pregunta secreta y respuesta secreta.
+    3. Guarda los datos del nuevo usuario en el archivo CSV.
+    '''
     try:
         print("Iniciando el registro de usuario")
-        user = input("Escriba un nombre de usuario: ").strip().lower()
+        user = request_input("Escriba un nombre de usuario: ").lower()
         while user in datos_usuarios["Usuarios"].str.lower().values:
             print("Ese nombre de usuario ya está seleccionado.")
-            user = input("Escriba un nombre de usuario diferente: ").strip().lower()
-        password = input("Escribe una contraseña segura: ").strip()
+            user = request_input("Escriba un nombre de usuario diferente: ").lower()
+        password = request_password("Escribe una contraseña segura (mínimo 8 caracteres alfanuméricos): ")
         password_hash = hashlib.sha256(password.encode('utf-8')).hexdigest()
-        secret_q = input("Escriba una pregunta que solo usted conozca la respuesta: ").strip().lower()
-        secret_a = hashlib.sha256(input("Escriba la respuesta a su pregunta secreta: ").strip().lower().encode('utf-8')).hexdigest()
+        secret_q = request_input("Escriba una pregunta que solo usted conozca la respuesta: ").lower()
+        secret_a = hashlib.sha256(request_input("Escriba la respuesta a su pregunta secreta: ").encode('utf-8')).hexdigest()
         nuevo_usuario = pd.DataFrame({'Usuarios': [user], 'Passwords': [password_hash], 'Pregunta Secreta': [secret_q], 'Respuesta Secreta': [secret_a]})
         datos_usuarios = pd.concat([datos_usuarios, nuevo_usuario], ignore_index=True)
         datos_usuarios.to_csv(path, index=False)
         print("Usuario registrado correctamente.")
-        registrar_accion(f"Nuevo usuario registrado: {user}")
+        register_action(f"Nuevo usuario registrado: {user}")
     except Exception as e:
         print(f"Ha ocurrido un error al registrar usuario: {e}")
-        registrar_accion(f"Error al registrar usuario: {e}")
-        
-def validar_contrasena(datos_usuarios, usuario): 
-    password_inp = input("Escriba su contraseña: ").strip()
-    # Control para que no introduzcan una contraseña vacía
-    while not password_inp:
-        print("La contraseña no puede estar vacía.")
-        password_inp = input("Escriba su contraseña: ").strip()
-    password_inp_hash = hashlib.sha256(password_inp.encode('utf-8')).hexdigest()
+        register_action(f"Error al registrar usuario: {e}")
+
+def validate_password(datos_usuarios, usuario): 
+    ''' 
+    Esta función valida que la contraseña introducida por el usuario es correcta.
+    En caso que no se introduzca la contraseña correcta en los 6 intentos, se pide al usuario que reinicie el programa
+    '''
     intentos = 0
     password_local = datos_usuarios.loc[datos_usuarios["Usuarios"].str.lower() == usuario, "Passwords"].values[0]
-
-    while password_inp_hash != password_local and intentos < 6:
-        print("Contraseña incorrecta")
-        registrar_accion(f"Contraseña incorrecta para usuario: {usuario}")
-        password_inp = input("Escriba su contraseña: ").strip()
-        while not password_inp:
-            print("La contraseña no puede estar vacía.")
-            password_inp = input("Escriba su contraseña: ").strip()
+    while intentos < 6:
+        password_inp = request_password("Escriba su contraseña: ")
         password_inp_hash = hashlib.sha256(password_inp.encode('utf-8')).hexdigest()
+        if password_inp_hash == password_local:
+            print("Bienvenido")
+            register_action(f"Usuario autenticado correctamente: {usuario}")
+            return
+        else:
+            print("Contraseña incorrecta")
+            register_action(f"Contraseña incorrecta para usuario: {usuario}")
         intentos += 1
+    print("Demasiados intentos fallidos, reinicie el programa")
 
-    if password_inp_hash == password_local:
-        print("Bienvenido")
-        registrar_accion(f"Usuario autenticado correctamente: {usuario}")
-        # Taximetro()
-    else:
-        print("Demasiados intentos fallidos, reinicie el programa")
-            
-def cambiar_contrasena(datos_usuarios, usuario):             
+def change_password(datos_usuarios, usuario): 
+    '''
+    Cambia la contraseña del usuario:
+    Verifica la respuesta a la pregunta secreta (hasta 3 intentos).
+    Solicita una nueva contraseña que debe ser diferente a la actual.
+    '''            
     secret_q = datos_usuarios.loc[datos_usuarios["Usuarios"].str.lower() == usuario, "Pregunta Secreta"].values[0]
-    secret_answ = hashlib.sha256(input(secret_q).encode('utf-8')).hexdigest()
     true_answ = datos_usuarios.loc[datos_usuarios["Usuarios"].str.lower() == usuario, "Respuesta Secreta"].values[0]
     intentos = 0
 
-    while secret_answ != true_answ and intentos < 3:
-        print("Respuesta incorrecta")
-        registrar_accion(f"Respuesta secreta incorrecta para usuario: {usuario}")
-        secret_answ = hashlib.sha256(input(secret_q).encode('utf-8')).hexdigest()
+    while intentos < 3:
+        secret_answ = hashlib.sha256(request_input(secret_q).encode('utf-8')).hexdigest()
+        if secret_answ == true_answ:
+            current_pass_hash = datos_usuarios.loc[datos_usuarios["Usuarios"].str.lower() == usuario, "Passwords"].values[0]
+            new_pass = request_password("Introduce tu nueva contraseña (mínimo 8 caracteres alfanuméricos): ")
+            new_pass_hash = hashlib.sha256(new_pass.encode('utf-8')).hexdigest()
+            while new_pass_hash == current_pass_hash:
+                print("La nueva contraseña no puede ser igual a la actual.")
+                new_pass = request_password("Introduce tu nueva contraseña diferente a la actual: ")
+                new_pass_hash = hashlib.sha256(new_pass.encode('utf-8')).hexdigest()
+            datos_usuarios.loc[datos_usuarios["Usuarios"].str.lower() == usuario, "Passwords"] = new_pass_hash
+            datos_usuarios.to_csv(path, index=False)
+            print("Contraseña cambiada")
+            register_action(f"Contraseña cambiada para usuario: {usuario}")
+            return
+        else:
+            print("Respuesta incorrecta")
+            register_action(f"Respuesta secreta incorrecta para usuario: {usuario}")
         intentos += 1
+    print("Demasiados intentos fallidos, reinicie el programa")
+    register_action(f"Demasiados intentos fallidos para cambiar contraseña de usuario: {usuario}")
 
-    if secret_answ == true_answ:
-        new_pass = input("Introduce tu nueva contraseña: ")
-        while not new_pass:
-            print("La contraseña no puede estar vacía.")
-            new_pass = input("Introduce tu nueva contraseña: ").strip()
-        current_pass_hash = datos_usuarios.loc[datos_usuarios["Usuarios"].str.lower() == usuario, "Passwords"].values[0]
-        new_pass_hash = hashlib.sha256(new_pass.encode('utf-8')).hexdigest()
-        while new_pass_hash == current_pass_hash:
-            print("La nueva contraseña no puede ser igual a la actual.")
-            new_pass = input("Introduce tu nueva contraseña diferente a la actual: ").strip()
-            while not new_pass:
-                print("La contraseña no puede estar vacía.")
-                new_pass = input("Introduce tu nueva contraseña: ").strip()
-            new_pass_hash = hashlib.sha256(new_pass.encode('utf-8')).hexdigest()        
-        datos_usuarios.loc[datos_usuarios["Usuarios"].str.lower() == usuario, "Passwords"] = new_pass_hash
-        datos_usuarios.to_csv(path, index=False)
-        print("Contraseña Cambiada")
-        registrar_accion(f"Contraseña cambiada para usuario: {usuario}")
-    else:
-        print("Demasiados intentos fallidos, reinicie el programa")
-        registrar_accion(f"Demasiados intentos fallidos para cambiar contraseña de usuario: {usuario}")
-
-def LogIn(cambio=False):
-    datos_usuarios = cargar_datos()
+def login_user(cambio=False):
+    '''
+    Gestiona el inicio de sesión o cambio de contraseña:
+    Carga los datos de usuarios.
+    Si cambio es True, cambia la contraseña; si no, valida la contraseña.
+    '''
+    datos_usuarios = load_data()
     try:
-        usuario = input("Escriba su nombre de usuario: ").lower()
-
+        usuario = request_input("Escriba su nombre de usuario: ").lower()
         if cambio:
             if usuario not in datos_usuarios["Usuarios"].str.lower().values:
                 print("Este usuario no existe")
-                registrar_usuario(datos_usuarios)
-                registrar_accion(f"Intento de (()(cambio de contraseña para usuario no existente: {usuario}")
+                register_user(datos_usuarios)
+                register_action(f"Intento de cambio de contraseña para usuario no existente: {usuario}")
             else:
-                cambiar_contrasena(datos_usuarios, usuario)
+                change_password(datos_usuarios, usuario)
         else:
             if usuario not in datos_usuarios["Usuarios"].str.lower().values:
                 print("Usuario no registrado.")
-                registrar_usuario(datos_usuarios)
+                register_user(datos_usuarios)
             else:
-                validar_contrasena(datos_usuarios, usuario)
+                validate_password(datos_usuarios, usuario)
     except Exception as e:
-        registrar_accion(f"Error: {e}")
-        registrar_accion(traceback.format_exc())
+        register_action(f"Error: {e}")
+        register_action(traceback.format_exc())
         print("Ha ocurrido un error. Por favor, revise el registro.")
 
-def Iniciar():
-    #LogIn(False)
-    LogIn(True)
-    
-Iniciar()
-#Iniciar(True)
+def start():
+    '''
+    Llama a la función login_user con el parámetro cambio
+    Si cambio=True para start el proceso de cambio de contraseña al ejecutar el programa.
+    Si cambio=False se inicia el proceso para start sesion en la aplicación
+    '''
+    #login_user(False)
+    login_user(True)
+
+start()

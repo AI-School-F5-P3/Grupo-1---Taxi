8a6e3e2cccb14e9db8e8c293fe72e06f84adb2ca
carrillo-p <alb.carrillop@gmail.com>
2024-06-23 16:04:33 +0200

    Eliminar pantallas innecesarias

--- IU_Class.py
+++ IU_Class.py
@@ -15,14 +15,10 @@ class Game:
         self.gameStateManager = gameStateManager('start')
         self.start = Start(self.screen, self.gameStateManager)
         self.taximetro = Taximetro(self.screen , self.gameStateManager)
-        self.login = login(self.screen, self.gameStateManager, self.manager)
-        self.registro = registro(self.screen, self.gameStateManager)
         self.quit = quit(self.gameStateManager)
 
         self.states = {'start': self.start, 
-                       'taximetro': self.taximetro, 
-                       'login': self.login,
-                       'registro': self.registro,
+                       'taximetro': self.taximetro,
                        'quit': self.quit}
 
     def run(self):
@@ -53,14 +49,11 @@ class Start:
         color_font = (200, 245, 10, 1)
         color_rect_hover = (91 ,23 ,202, 0.8)
         color_rect_base = (65, 0, 168, 0.9)
-        #Boton Login
-        login_button_rect = pygame.Rect(220, 50, 390, 55)
-        login_text = font.render('Iniciar Sesion', True, color_font)
-        #Boton Sign Up
-        reg_button_rect = pygame.Rect(260, 150, 305, 55)
-        reg_text = font.render('Registrarse', True, color_font)
+        #Boton Start
+        login_button_rect = pygame.Rect(225, 150, 415, 55)
+        login_text = font.render('Empezar carrera', True, color_font)
         #Boton Quit
-        quit_button_rect = pygame.Rect(360, 250, 120, 55)
+        quit_button_rect = pygame.Rect(375, 250, 120, 55)
         quit_text = font.render('Quit', True, color_font)
         self.display.blit(login_screen, (0,0))
         if quit_button_rect.x <= a <= quit_button_rect.x+120 and quit_button_rect.y <= b <= quit_button_rect.y+55:
@@ -68,18 +61,12 @@ class Start:
         else:
             pygame.draw.rect(self.display, color_rect_base, quit_button_rect)
 
-        if reg_button_rect.x <= a <= reg_button_rect.x+305 and reg_button_rect.y <= b <= reg_button_rect.y+55:
-            pygame.draw.rect(self.display, color_rect_hover, reg_button_rect)
-        else:
-            pygame.draw.rect(self.display, color_rect_base, reg_button_rect)
-
-        if login_button_rect.x <= a <= login_button_rect.x+390 and login_button_rect.y <= b <= login_button_rect.y+55:
+        if login_button_rect.x <= a <= login_button_rect.x+415 and login_button_rect.y <= b <= login_button_rect.y+55:
             pygame.draw.rect(self.display, color_rect_hover, login_button_rect)
         else:
             pygame.draw.rect(self.display, color_rect_base, login_button_rect)   
 
         self.display.blit(login_text, (login_button_rect.x + 5, login_button_rect.y+5))
-        self.display.blit(reg_text, (reg_button_rect.x + 5, reg_button_rect.y + 5))
         self.display.blit(quit_text, (quit_button_rect.x + 5, quit_button_rect.y + 5))
         
         if keys[pygame.K_e]:
@@ -88,9 +75,7 @@ class Start:
             if quit_button_rect.collidepoint((a,b)):
                 self.gameStateManager.set_state('quit')
             if login_button_rect.collidepoint((a, b)):
-                self.gameStateManager.set_state('login')
-            if reg_button_rect.collidepoint((a, b)):
-                self.gameStateManager.set_state('registro')
+                self.gameStateManager.set_state('taximetro')
         
 class Taximetro:
     def __init__(self, display, gameStateManager):
@@ -100,31 +85,6 @@ class Taximetro:
     def run(self):
         first_screen = pygame.image.load('Graficos/start.jpg')
         self.display.blit(first_screen, (0,0))
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_q]:
-            self.gameStateManager.set_state('start')
-
-class login:
-    def __init__(self, display, gameStateManager, manager):
-        self.display = display
-        self.gameStateManager = gameStateManager
-        self.manager = manager
-
-    def run(self):
-        login = pygame.image.load('Graficos/login.jpg')
-        self.display.blit(login, (0,0))
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_q]:
-            self.gameStateManager.set_state('start')
-
-class registro:
-    def __init__(self, display, gameStateManager):
-        self.display = display
-        self.gameStateManager = gameStateManager
-    
-    def run(self):
-        registro = pygame.image.load('Graficos/registro (Mediana).jpeg')
-        self.display.blit(registro, (0,0))
         keys = pygame.key.get_pressed()
         if keys[pygame.K_q]:
             self.gameStateManager.set_state('start')
@@ -144,6 +104,9 @@ class quit:
         pygame.quit()
         exit()
 
+
+game = Game()
+game.run()
 
 def init_game():
     game = Game()

478ee013da283c97777e53287aab731b85bb4605
carrillo-p <alb.carrillop@gmail.com>
2024-06-23 14:43:51 +0200

    Añadido ejecución de pygame

--- Check_passwords.py
+++ Check_passwords.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import hashlib
+import tkinter as tk
+from IU_Class import init_game
 
 #Cuando tengamos una base de datos como tal
 #datos_usuarios = pd.read_csv("usuarios.csv")
@@ -8,12 +10,11 @@ import hashlib
 def LogIn(username, password):    
     datos_usuarios = pd.read_csv("Usuarios.csv")
     password_inp = hashlib.sha256(password.encode('utf-8')).hexdigest()
-    password_local = datos_usuarios.query('Usuarios == @usuario')["Passwords"][1]
+    password_local = datos_usuarios.loc[datos_usuarios["Usuarios"] == username]["Passwords"][1]
 
     if username not in datos_usuarios["Usuarios"].values or password_inp != password_local:
-        messagebox.showinfo(title = "Error", message = "Usuario o contraseña incorrecta")
+        tk.messagebox.showinfo(title = "Error", message = "Usuario o contraseña incorrecta")
     
     else:
-        game = Game()
-        game.run()
+        init_game()
 

951c840dfcb93d6f7d861a0ac8b336199acc4cb9
carrillo-p <alb.carrillop@gmail.com>
2024-06-23 15:12:34 +0200

    Funcionalidad de registro

--- Check_passwords.py
+++ Check_passwords.py
@@ -10,7 +10,7 @@ from IU_Class import init_game
 def LogIn(username, password):    
     datos_usuarios = pd.read_csv("Usuarios.csv")
     password_inp = hashlib.sha256(password.encode('utf-8')).hexdigest()
-    password_local = datos_usuarios.loc[datos_usuarios["Usuarios"] == username]["Passwords"][1]
+    password_local = datos_usuarios.loc[datos_usuarios["Usuarios"] == username]["Passwords"].item()
 
     if username not in datos_usuarios["Usuarios"].values or password_inp != password_local:
         tk.messagebox.showinfo(title = "Error", message = "Usuario o contraseña incorrecta")
@@ -18,3 +18,15 @@ def LogIn(username, password):
     else:
         init_game()
 
+def Register(username, password, s_quest, s_answer):
+    datos_usuarios = pd.read_csv("Usuarios.csv")
+    if datos_usuarios.Usuarios.isin([username]).any():
+        tk.messagebox.showinfo(title = "Error", message = "Nombre de usuario en uso, por favor eliga otro")
+    else:
+        password_hash = hashlib.sha256(password.encode('utf-8')).hexdigest()
+        df = pd.DataFrame({'Usuarios' : [username], 'Passwords': [password_hash], 'Pregunta Secreta' : s_quest, 'Respuesta Secreta': s_answer})
+        datos_usuarios = pd.concat([datos_usuarios, df], ignore_index = True)
+        datos_usuarios.to_csv('Usuarios.csv', index = False)
+        tk.messagebox.showinfo(title = "Registro completado", message = "Registo completado, por favor cierre la aplicación e inicie sesión")
+        return(True)
+                        

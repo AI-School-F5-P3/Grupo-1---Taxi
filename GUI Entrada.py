c2f1d682d351aee8421e5dbf76c6191be5638c80
carrillo-p <alb.carrillop@gmail.com>
2024-06-25 13:13:10 +0200

    Boton de ayuda con texto readme

--- GUI Entrada.py
+++ GUI Entrada.py
@@ -29,6 +29,9 @@ class GUI:
         self.reg = tk.Button(self.root, text="Registrarse", command=self.reg_screen, **self.style_button)
         self.reg.pack(pady=75)
 
+        self.reg = tk.Button(self.root, text="Ayuda", command=self.pantalla_ayuda, **self.style_button)
+        self.reg.pack(pady=75)
+
     def clear_screen(self):
         for widget in self.root.winfo_children():
             widget.destroy()
@@ -109,7 +112,36 @@ class GUI:
         button_frame.pack(pady=20)
 
         logger.info('Pantalla de Registro de Usuarios') # Control de Log
-    
+
+    def pantalla_ayuda(self):
+        self.clear_screen()
+
+        texto = """
+Como arrancar el programa
+
+Hay que bajarse del github todos los documentos que hay subidos y mantener la misma estructura que tienen.
+
+Para poder ejecutar el código se necesita instalar las bibliotecas:
+
+    -tkinter
+    -pygame
+    -pygame_gui
+
+El codigo incluye un archivo txt que puede ejecutarse con pip para instalar todos los paquetes necesarios en una sola linea: 'pip install -r "ruta/del/arhcivo/requirements.txt" Si se utiliza Anaconda o Miniconda para python puede instalarse pip en el entorno virtual y despues ejecutar el código previo. No obstante conda no recomienda este método, revisar documentación que se ajuste a cada caso de uso.
+
+Ejecutar el script 'GUI Entrada.py' para comenzar a utilizar la aplicación y seguir los pasos necesarios. Registrarse si es la primera vez y seleccionar tipo de conductor.
+
+Despues cuando el inicio de sesión es correcto y no presenta errores se podrán definir los descuentos que se quieran aplicar en el caso de licencias 'VTC' y seleccionar el turno en el caso de Taxistas. La tarifa extra solo se aplica durante el horario nocturno pero de momento hay que escribirla hasta que se meta un valor de control ahí para que no salte error.
+"""
+        texto_widget = tk.Text(self.root, wrap=tk.WORD, bg="#541388", fg="#C8F50A", font=("Lucida Console", 16))
+        texto_widget.pack(padx=20, pady=40)
+
+        # Insertar el texto
+        texto_widget.insert(tk.END, texto)
+
+        # Deshabilitar la edición del texto (opcional)
+        texto_widget.configure(state='disabled')
+
     def res_pswd(self):
         self.clear_screen()
         

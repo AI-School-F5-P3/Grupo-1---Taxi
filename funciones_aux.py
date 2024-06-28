f10e9b710e013b369ef56ecaaa5f871af6b9a459
carrillo-p <alb.carrillop@gmail.com>
2024-06-28 18:51:34 +0200

    añadidos comentarios explicativos

--- funciones_aux.py
+++ funciones_aux.py
@@ -20,7 +20,7 @@ def LogIn(username, password):
     '''
     Función para iniciar sesión en la interfaz gráfica de tkinter.
     En primer lugar se intenta capturar el error de que la base de datos no pueda accederse, generalmente se debe a que esté abierta a la vez que se intenta editar.
-    En segundo lugar, para asegurar que el formato siempre es igual, se convierte el nombre de usuario a minúsuclas, y se codifica la contraseña en .sha256.
+    En segundo lugar, para asegurar que el formato siempre es igual, se convierte el nombre de usuario a minúsuclas, y se codifica la contraseña se ecnripta usando el algoritmo sha-256.
     En tercer lugar se intenta catpurar el error de que la base de que el usuario no se encuentre en la base de datos. Para ello se especifican dos errores:
         - ValueError. El valor introducido es inocrrecto.
         - IndexError: No se encuentra en el índice el usuario por lo que genera un error de índice (pandas no puede encontrar la instancia en la base).
@@ -56,9 +56,20 @@ def LogIn(username, password):
         return user_info["Licencia"]
 
 def LogIn_Empresa(username, password):
+    '''
+    Función para iniciar sesión en la interfaz gráfica de tkinter.
+    En primer lugar se intenta capturar el error de que la base de datos no pueda accederse, generalmente se debe a que esté abierta a la vez que se intenta editar. O que no haya un archivo porque no se ha registrado ninguna empresa en este momento.
+    En segundo lugar, para asegurar que el formato siempre es igual, se convierte el nombre de usuario a minúsuclas, y se codifica la contraseña se ecnripta usando el algoritmo sha-256.
+    En tercer lugar se intenta catpurar el error de que la base de que el usuario no se encuentre en la base de datos. Para ello se especifican dos errores:
+        - ValueError. El valor introducido es inocrrecto.
+        - IndexError: No se encuentra en el índice el usuario por lo que genera un error de índice (pandas no puede encontrar la instancia en la base).
+    A continuación, una vez se solventan los errores, se comprueba si hay errores en el nombre de usuario o en la contraseña, en cuyo caso el inicio de sesión falla.
+    Si todo ha salido bien, el inicio de sesión ha sido exitoso y se extrae la empresa correspondiente para ese usuario para su uso en la función principal.
+    Todos los pasos se registran en el archivo de log.
+    '''
     username = username.lower()
     try:
-        datos_empresa = pd.read_csv("Empresa.csv")
+        datos_empresa = pd.read_csv(Empresa_DB)
     except PermissionError:
         logger.warning('No se tienen permisos para acceder a la base de datos.')
         #messagebox.showinfo(title="Error", message="No se tienen permisos para acceder a la base de datos.")
@@ -90,6 +101,13 @@ def LogIn_Empresa(username, password):
 
 
 def Register(username, password, s_quest, s_answer, conductor, empresa):
+    '''
+    Función para registrar un conductor en la base de datos.
+    En primer lugar se intenta capturar el error de que la base de datos no pueda accederse, generalmente se debe a que esté abierta a la vez que se intenta editar.
+    En segundo lugar, para asegurar que el formato siempre es igual, se convierte el nombre de usuario, respuesta secreta y nombre de empresa a minúsuclas.
+    En trecer lugar se incluyen una serie de controles para garantizar que se han rellenado todos los espacios en el registro de usuario y no se han dejado datos vacíos o con valores no válido (p.ej. la contraseña tiene que tener mínimo 4 caracteres).
+    Una vez se han llevado a cabo estos controles, si se han resuelto satisfactoriamentes, se encripta la contraseña del usuario utilizando el algoritmo sha-256 y se genera una base de datos temporal con pandas incluyendo todos los datos del usuario. Despúes se concatena a la base de datos de usuarios y se guarda el archivo .csv con el mismo nombre, de esta forma actualizamos con cada registro la base de datos.
+    '''
     try:
         datos_usuarios = pd.read_csv(User_DB)
     except PermissionError:
@@ -140,9 +158,15 @@ def Register(username, password, s_quest, s_answer, conductor, empresa):
 
 
 def Pregunta(username):
+    '''
+    Función para recuperar la pregunta secreta del usuario. 
+    En primer lugar se convierte el nombre de usuario a mínusculas, formato en el que están los usuarios en la base de datos.
+    Se capturan errores de no haber introducido un nombre de usuario o de que el usuario no exista en nuestra base de datos. 
+    Si todo está en orden se muestra la pregunta secreta al usuario utilizando un pop-up definido en messagebox.showinfo de la biblioteca tkinter.
+    '''
     username = username.lower()
 
-    datos_usuarios = pd.read_csv("Usuarios.csv")
+    datos_usuarios = pd.read_csv(User_DB)
 
     if not username:
         messagebox.showinfo(title="Error", message="Debe introducir un nombre de usuario.")
@@ -157,6 +181,13 @@ def Pregunta(username):
         messagebox.showinfo(title = "Pregunta", message = pregunta_s)
 
 def Respuesta(username, answer, new_pswd):
+    '''
+    Función que comprueba que el usuario haya introducido la respuesta secreta correcta. Se capturan los errores de no poder acceder a la base de datos (probablemente porque en ese momento esté abierta).
+    Se convierten el usuario y la respuesta a mínusculas, formato en el que se encuentran en la base de datos y se comprueba que los formularios no estén vacíos.
+    Si no hay errores, se comprueba que el usuario exista y que la respuesta introducida corresponde a aquella que se incluye en la base de datos para dicho usuario.
+    Si se cumplen ambas condiciones se acepta la contraseña nueva que el usuario haya indicado (siempre que cumpla tener más de 4 caracteres), se encripta con sha-256 y se actualiza en la base de datos.
+    Si ha habido errores al introducir la respuesta secreta, se avisa al usuario mediante un pop-up.
+    '''
     try:
         datos_usuarios = pd.read_csv(User_DB)
     except PermissionError:
@@ -189,7 +220,6 @@ def Respuesta(username, answer, new_pswd):
         logger.error('Usuario no encontrado o respuesta incorrecta.')
         return False
 
-    #local_answ = datos_usuarios.loc[datos_usuarios["Usuarios"] == username]["Respuesta Secreta"].item()
     if local_answ == answer:
         if len(new_pswd) < 4:
             messagebox.showinfo(title="Error", message="La contraseña debe tener al menos 4 caracteres.")
@@ -212,6 +242,9 @@ def Respuesta(username, answer, new_pswd):
         logger.error('Intento de recuperación de contraseña fallida: Respuesta Secreta Incorrecta')
         
 def Descuentos(username, stop_disc, mov_disc): 
+    '''
+    Función que aplica, para el usuario con licencia VTC logeado, los descuentos que haya indicado en la pantalla de descuentos. Estos se guardan en formato int(entero) para poder trabajar con ellos correctamente en pygame. Se capturan además los errores de que no se tenga permiso de acceso a la base de datos o de que no se haya incluido un número entero. Podría trabajarse con numeros decimales, pero consideramos que es poco probable que alguien quiera hacer un descuento porcentual con decimales.
+    '''
     datos_usuarios = pd.read_csv("Usuarios.csv") 
     username = username.lower()
         
@@ -231,6 +264,9 @@ def Descuentos(username, stop_disc, mov_disc):
 
 
 def Descuentos_taxi(username, turno, tarifa): 
+    '''
+    Función que aplica, para el usuario con licencia de taxi logeado, la tarifa que haya señalado en la pantalla de tarifa. Este se guarda en formato int(entero) para poder trabajar correctamente en pygame. Se capturan además los errores de que no se tenga permiso de acceso a la base de datos o de que no se haya incluido un número entero. Podría trabajarse con numeros decimales, pero consideramos que es poco probable que alguien quiera hacer un descuento porcentual con decimales.
+    '''
     datos_usuarios = pd.read_csv("Usuarios.csv") 
     username = username.lower()
     try:
@@ -248,7 +284,24 @@ def Descuentos_taxi(username, turno, tarifa):
         return False
     
 def Tarifa(empresa, tarifa_mov, tarifa_stp):
-    datos_usuarios = pd.read_csv("Usuarios.csv")
+    '''
+    Función que aplica las tarifas que haya indicado el representante de la empresa para todos los usuarios de dicha empresa. 
+    Se comprueba que el valor sea númerico intentando convertirlo a entero (los inputs son siempre strings) en un try except. Si no se puede se devuelve el mensaje en pop-up de que tiene que introducirse un valor numérico.
+    Se incluye además un breve condicional para que, si se incluye una tarifa con valor negativo se convierta al valor mínimo posible, un céntimo por segundo (0.01)
+    Por último se captura el error de que no se tengan permisos de acceso a la base de datos.
+    '''
+    try:
+        int(tarifa_mov)
+        int(tarifa_stp)
+    except ValueError:
+        logger.error('No se ha introducido un número como valor')
+        messagebox.showinfo(title="Error", message="Ha de introducirse un número")
+        return False
+
+    tarifa_mov = tarifa_mov if tarifa_mov >= 0.01 else 0.01
+    tarifa_stp = tarifa_stp if tarifa_stp >= 0.01 else 0.01
+
+    datos_usuarios = pd.read_csv(User_DB)
     try:
         datos_usuarios.loc[datos_usuarios["Empresa"] == empresa, "Tarifa Mov"] = float(tarifa_mov)
         datos_usuarios.loc[datos_usuarios["Empresa"] == empresa, "Tarifa Stop"] = float(tarifa_stp)

850460255c898ad1fe24a227021f80315b4a3a0c
carrillo-p <alb.carrillop@gmail.com>
2024-06-28 11:20:35 +0200

    explicacion del logger

--- logger_config.py
+++ logger_config.py
@@ -2,6 +2,17 @@ import logging
 import os
 
 def setup_logger():
+    '''
+    La función en primer lugar comprueba que estemos en la carpeta de trabajo correcta, en nuestro caso aquella en la que se encuentra la función main.py.
+    Si no estamos en la carpeta adecuada se modifica automáticamente.
+    Se inicializa el gestor de logs en la variable 'logger' utilizando la función getlogger de la biblioteca logging.
+    Después se genera el 'gestor' para ir introduciendo los logs en un archivo externo, definido como 'taxi_app_logger.log' y que utilizará la codificación 'utf-8'
+    Se especifica además el nivel (entendiendo nivel como la gravedad que presenta el mensaje) minimo que captura el logger. Con DEBUG capturamos todos los niveles más graves que DEBUG.
+    Se añade también el 'gestor' para que los mensajes aparezcan además de en el archivo externo, en la consola a medida que se vaya ejecutando el programa.
+    Posteriormente se especifica el formato en el que queremos que se generen los logs, en este caso 'asctime' define el formato de fecha (con fecha actual y hora, levelname define el nivel de gravedad del mensaje, y message muestra el mensaje que se ha especificado que se quiere loggear cuando se da la condición x.)
+    Finalmente se añaden los gestores al 'logger' y el return devuelve el logger para utilizarlo en el cuerpo principal de la apliación.
+    '''
+    
     for dirpath, dirnames, filenames in os.walk("."):
         for filename in [f for f in filenames if f.endswith("funciones_aux.py")]:
             os.chdir(dirpath)
@@ -22,7 +33,7 @@ def setup_logger():
     ch.setLevel(logging.ERROR)
     
     # Definir el formato del log
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
     fh.setFormatter(formatter)
     ch.setFormatter(formatter)
     
